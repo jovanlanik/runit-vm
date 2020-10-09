6a2a52213c7577ed5ac0255f77c7e3938c7c1d07
Jovan Lanik <jox969@gmail.com>
2020-10-09 13:16:39 +0200

    hide default directory

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 [ -z $VMDIR ] && VMDIR="$HOME/.local/vms"
 echo "Installing to $VMDIR..."
 [ ! -d "$VMDIR" ] && mkdir -p $VMDIR
-[ ! -e "$VMDIR/default" ] && ln -s $(realpath ./default) "$VMDIR/default"
+[ ! -e "$VMDIR/default" ] && ln -s $(realpath ./default) "$VMDIR/.default"
 echo "Done."
